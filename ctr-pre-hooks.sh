3ba857807b6a8492a53eb9bfa9db7539c9e24587
Isabelle Jackson <izzyjackson05@outlook.com>
2026-06-18 23:35:52 +0100

    fix: use upstream hostname

--- ctr-pre-hooks.sh
+++ ctr-pre-hooks.sh
@@ -2,9 +2,6 @@
 
 # Commands in this script are ran in the local container image before anything else is ran. This is used to apply ISO-specific changes, such as autologin and installer setup. 
 
-# Set hostname to apollolive
-rm -f /etc/hostname && echo apollolive > /etc/hostname
-
 # Apply autologin settings to GDM
 tee -a /etc/gdm/custom.conf <<EOL
 [daemon]
